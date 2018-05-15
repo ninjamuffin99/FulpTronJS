330ac2f9ccf26328ded070a53c6e90c8633d7556
Cameron <cameron.taylor.ninja@gmail.com>
2018-05-15 09:35:45 -0700

    got config going

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require('discord.js');
 // create a new Discord client
 const client = new Discord.Client();
 
+const config = require('./config.json');
+
 // when the client is ready, run this code
 // this event will trigger whenever your bot:
 // - finishes logging in
@@ -12,8 +14,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	console.log(message.content);
+	if (message.content === 'fulpPing')
+	{
+		// var emoji = Discord.emoji.from
+
+		// message.channel.send($'{emoji} Pong! Ping: {client.ping}ms');
+	}
 });
 
 // login to Discord with your app's token
-client.login('token goes here lol');
+client.login(config.token);
