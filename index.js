2cac6f795d0f4c306813d7a58e10ea1ed8e8c0f2
Cameron <cameron.taylor.ninja@gmail.com>
2018-05-15 10:38:09 -0700

    shit man idk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 // create a new Discord client
 const client = new Discord.Client();
-
+const ytdl = require('ytdl-core');
 const config = require('./config.json');
 
 // when the client is ready, run this code
@@ -14,12 +14,40 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === 'fulpPing')
-	{
+	if (message.content === 'fulpPing') {
 		// var emoji = Discord.emoji.from
 
 		// message.channel.send($'{emoji} Pong! Ping: {client.ping}ms');
 	}
+
+	if (message.content === 'fulpPlaying') {
+			client.user.setActivity('coolmathgames.com', { type: 'PLAYING' })
+			.then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+  .catch(console.error);
+	}
+
+	if (message.content === 'fulpPlay') {
+		if (message.channel.type !== 'text') return;
+
+		const { voiceChannel } = message.member;
+
+		if (!voiceChannel) {
+			return message.reply('Please join a voice channel first!');
+		}
+
+		voiceChannel.join().then(connection => {
+			const stream = ytdl('https://www.youtube.com/watch?v=09R8_2nJtjg', { filter: 'audioonly' });
+			const dispatcher = connection.playStream(stream);
+
+			dispatcher.on('end', () => voiceChannel.leave());
+		});
+
+	}
+
+	if (message.content == 'fulpReboot') {
+
+	}
+
 });
 
 // login to Discord with your app's token
