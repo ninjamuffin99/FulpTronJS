1ded39b04f6e7d4d26bf5651ad1ebdd06d755a33
Cameron <cameron.taylor.ninja@gmail.com>
2018-05-15 14:43:29 -0700

    server info command

--- index.js
+++ index.js
@@ -3,30 +3,35 @@ const Discord = require('discord.js');
 // create a new Discord client
 const client = new Discord.Client();
 const ytdl = require('ytdl-core');
-const config = require('./config.json');
+const { prefix, token } = require('./config.json')
 
 // when the client is ready, run this code
 // this event will trigger whenever your bot:
 // - finishes logging in
 // - reconnects after disconnecting
-client.on('ready', () => {
+client.on('ready', () => 
+{
 	console.log('Ready!');
 });
 
-client.on('message', message => {
-	if (message.content === 'fulpPing') {
+client.on('message', message => 
+{
+	if (message.content === 'fulpPing') 
+	{
 		// var emoji = Discord.emoji.from
 
 		// message.channel.send($'{emoji} Pong! Ping: {client.ping}ms');
 	}
 
-	if (message.content === 'fulpPlaying') {
+	if (message.content === 'fulpPlaying') 
+	{
 			client.user.setActivity('coolmathgames.com', { type: 'PLAYING' })
 			.then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-  .catch(console.error);
+  			.catch(console.error);
 	}
 
-	if (message.content === '!play') {
+	if (message.content === '!play') 
+	{
 					if (message.channel.type !== 'text') return;
 
 					const { voiceChannel } = message.member;
@@ -41,13 +46,14 @@ client.on('message', message => {
 
 	            dispatcher.on('end', () => voiceChannel.leave());
 	        });
-	    }
-
-	if (message.content == 'fulpReboot') {
-		
 	}
 
+	// STUPID JS NOTE: MAKE SURE YOU USE ` BACKTICKS LIKE THIS, INSTEAD OF ' APOSTROPHES LIKE THIS
+	// IF YOU WANT TO USE EZ VARIABLES AND SHIT
+	if (message.content.toLowerCase().startsWith(`${prefix}server`)) {
+		message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nServer Region: ${message.guild.region}`);
+	}
 });
 
 // login to Discord with your app's token
-client.login(config.token);
+client.login(token);
