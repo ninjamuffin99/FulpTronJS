c2185d4ca59de359f28498da927e3e559f4c549e
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2019-05-26 15:09:57 -0700

    prep

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ const ytdl = require('ytdl-core');
 
 // NOTE IMPORTANT READ THIS
 // This line is commented in the master/heroku version, but it is needed if you were to run the code locally
-//const { prefix, token, ownerID, NGappID, NGencKey, GOOGLE_API_KEY, MMappID} = require('./config.json');
+const { prefix, token, ownerID, NGappID, NGencKey, GOOGLE_API_KEY, MMappID} = require('./config.json');
 
-let gCreds = require('./fulpGdrive.json');
-//let gCreds = require('./config.json');
-
+//let gCreds = require('./fulpGdrive.json');
+let gCreds = require('./config.json');
+/*
 const prefix = process.env.prefix;
 const ownerID = process.env.ownerID;
 const token = process.env.token;
@@ -40,7 +40,7 @@ const MMappID = process.env.MMappID;
 
 gCreds.private_key_id = process.env.private_key_id;
 gCreds.private_key = process.env.private_key;
-
+*/
 
 
 
