a61d8c07c6ce236e379c3e7b2c3ab5305d4b5481
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2019-05-27 10:51:48 -0700

    wack

--- index.js
+++ index.js
@@ -35,8 +35,10 @@ const NGencKey = process.env.NGencKey;
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 const MMappID = process.env.MMappID;
 
-gCreds.private_key_id = process.env.private_key_id.replace(/\\n/g, '\n');
+gCreds.private_key_id = process.env.private_key_id;
 gCreds.private_key = process.env.private_key;
+
+gCreds = gCreds.replace(/\\n/g, '\n');
 
 console.log(gCreds);
 
