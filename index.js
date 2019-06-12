2e4ce159dd28e8c79beb3de211738493a9511a14
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2019-06-12 16:07:30 -0700

    blamemd one

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ client.on('ready', () =>
 });
 
 let ngRef = ['Cock joke. username is here', 'username, just do what comes natural -T', 'le username has arrived', 'username, do you remember what a tardigrade is?',
-			'Angels sang out in an immaculate chorus, down from the heavends defended username'];
+			'Angels sang out in an immaculate chorus, down from the heavends defended username', 'username was balmmed for this post'];
 
 client.on('guildMemberAdd', async member =>
 {
