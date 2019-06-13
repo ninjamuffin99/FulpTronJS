2c8a858d11cbc95eb9c8bac8e616d430ba392770
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2019-06-12 20:20:08 -0700

    lol console.log instead of print()

--- index.js
+++ index.js
@@ -54,11 +54,11 @@ let shoomOCound = 2;
 async function prepPics()
 {
 	await getImages('fulp');
-	await print('Fulp shit');
+	await console.log('Fulp shit');
 	await getImages('dogl');
-	await print('dogl shit');
+	await console.log('dogl shit');
 	await getImages('delete');
-	await print('delete shit');
+	await console.log('delete shit');
 }
 
 // when the client is ready, run this code
