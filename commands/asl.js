72a7484f81d9dde25a7901fee68f7be046142b07
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-12-24 15:53:57 -0500

    asl implemented

---
+++ commands/asl.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('asl')
+        .setDescription('asl ;))')
+        .addIntegerOption(age =>
+            age.setName('age')
+            .setDescription('age???')
+            .setRequired(true)
+        )
+        .addStringOption(sex =>
+            sex.setName('sex')
+            .setDescription('sex? (yes)')
+            .setRequired(true)
+        )
+        .addStringOption(location =>
+            location.setName('location')
+            .setDescription('location???')
+            .setRequired(true)
+        ),
+    async execute(interaction) {
+        const a = interaction.options.getInteger('age');
+        const s = interaction.options.getString('sex');
+        const l = interaction.options.getString('location');
+
+        await interaction.reply(`Hello ${interaction.member.displayName}, I see you're a ${a} year old ${s} from ${l}. Wanna date?`);
+    }
+}
+
+/*
+.addIntegerOption(age => {
+            age.setName('age')
+                .setDescription('age???')
+                .setRequired(true)
+        })
+        .addStringOption(sex => {
+            sex.setName('sex')
+                .setDescription('sex? (yes)')
+                .setRequired(true)
+        })
+        .addStringOption(location => {
+            location.setName('location')
+                .setDescription('location???')
+                .setRequired(true)
+        })
+*/
